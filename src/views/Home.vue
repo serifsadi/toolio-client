d41a24778f8cc926312df49716b72493e0b98f92
Serif Sadi Senkule <serifsadi88@gmail.com>
2021-03-13 16:38:59 +0300

    Edited home view design.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
-    <SearchBar placeholder="Try Awesome" @onSearch="search"></SearchBar>
+    <v-container fill-height fluid>
+      <v-row align="center" justify="center">
+        <v-img
+          alt="Shopify Logo"
+          class="shrink"
+          position="center center"
+          contain
+          src="../assets/logo-shopify.png"
+          transition="scale-transition"
+          width="122"
+        />
+      </v-row>
+      <v-row>
+        <SearchBar placeholder="Try Awesome" @onSearch="search"></SearchBar>
+      </v-row>
+    </v-container>
   </div>
 </template>
 

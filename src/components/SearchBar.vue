9fb83fee9dc339d5e95b1a730bd2abe535d58f96
Serif Sadi Senkule <serifsadi88@gmail.com>
2021-03-13 16:37:00 +0300

    SearchBar component has been created and added to the home view.

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row>
+      <v-text-field
+        v-model="keyword"
+        v-bind:label="label || 'Search'"
+        v-bind:placeholder="placeholder"
+        append-icon="mdi-magnify"
+        filled
+        rounded
+        @keydown.enter="search()"
+      ></v-text-field>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  props: {
+    label: String,
+    placeholder: String
+  },
+  data: () => ({
+    keyword: ""
+  }),
+  methods: {
+    search() {
+      if (this.keyword !== "") {
+        this.$emit("onSearch", this.keyword);
+      }
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <SearchBar placeholder="Try Awesome" @onSearch="search"></SearchBar>
+  </div>
 </template>
 
 <script>
+import SearchBar from "@/components/SearchBar.vue";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    SearchBar
+  },
+  methods: {
+    search(keyword) {
+      console.log("search keyword", keyword);
+    }
+  }
 };
 </script>
